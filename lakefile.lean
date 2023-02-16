8e597ce11091663279dc185368346e050f82c72f
Suryak25 <suryak@iisc.ac.in>
2023-02-16 13:22:06 +0530

    Setup Done

--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,4 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «CubicFormula» {
-  -- add any library configuration options here
-}
+lean_lib LowDegreeEquations
